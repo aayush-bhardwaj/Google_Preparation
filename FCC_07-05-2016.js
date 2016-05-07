61e8889dcea7782dc1e6766ef416ae18fd429d1d
AAYUSH BHARDWAJ <aayush.bhj@gmail.com>
2016-05-07 18:39:53 +0530

    Update FCC_07-05-2016.js

--- FCC_07-05-2016.js
+++ FCC_07-05-2016.js
@@ -9,3 +9,19 @@ var expression = /and/gi;  // Change this Line
 
 var andCount = testString.match(expression).length;
 
+
+//Constructor Functions 
+
+var Bike = function() {
+  var gear;
+  
+  this.getGear = function(){
+    return gear;
+  };
+  
+  this.setGear = function(change){
+    gear = change;
+  };
+};
+var myBike = new Bike();
+
